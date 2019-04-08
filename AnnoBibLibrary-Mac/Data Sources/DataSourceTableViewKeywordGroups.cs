a3b05092a96c085d543de4872e7a96f3d63bf848
cschmid92 <cschmid92@gmail.com>
2019-04-08 16:54:44 -0700

    Readded mac project - built gui

---
+++ AnnoBibLibrary-Mac/Data Sources/DataSourceTableViewKeywordGroups.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using AppKit;
+
+namespace AnnoBibLibraryMac.DataSources
+{
+    public class DataSourceTableViewKeywordGroups : NSTableViewDataSource
+    {
+        public List<string> Keywords = new List<string>();
+
+        public override nint GetRowCount(NSTableView tableView)
+        {
+            return Keywords.Count;
+        }
+    }
+}
