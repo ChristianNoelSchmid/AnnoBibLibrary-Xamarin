e19d2e3ba6a0e23d14e67523c313c131f3ec7de6
cschmid92 <cschmid92@gmail.com>
2019-04-10 22:38:59 -0700

    Worked on Keyword Groups

---
+++ AnnoBibLibrary-Mac/PreferencesViewController.designer.cs
@@ -0,0 +1,32 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace AnnoBibLibraryMac
+{
+    [Register ("PreferencesViewController")]
+    partial class PreferencesViewController
+    {
+        [Outlet]
+        AppKit.NSTableView TableViewKeywordGroups { get; set; }
+
+        [Action ("OnAccept:")]
+        partial void OnAccept (Foundation.NSObject sender);
+
+        [Action ("OnAddKeywordGroup:")]
+        partial void OnAddKeywordGroup (Foundation.NSObject sender);
+        
+        void ReleaseDesignerOutlets ()
+        {
+            if (TableViewKeywordGroups != null) {
+                TableViewKeywordGroups.Dispose ();
+                TableViewKeywordGroups = null;
+            }
+        }
+    }
+}
